0d90e346743697a6f2286b2c5572faba126c8b36
Dhruv Singhal <108335481+Dhruvcodelover@users.noreply.github.com>
2024-03-30 10:55:20 +0000

    Datatype

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";   // Treat all JS code as newer version
+
+// alert(3 + 3)  // we are using nodejs, not browser
+console.log(3 
+    + 
+    3)  // code readability should be high
+
+console.log("Dhruv")
+
+let name = "Dhruv"
+let age = 20
+let loggedIn = true
+
+// number => Range 2^53
+//bigint => use by reddit,Ecommerce large site
+//boolean => true/false
+//string => ""
+// null => standAlone Value
+//undefined => Declare variable but not define
+
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined
+
+
