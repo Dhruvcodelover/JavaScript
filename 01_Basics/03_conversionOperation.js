3353115b3f2c09666dc3cb782242b08826082128
Dhruv Singhal <108335481+Dhruvcodelover@users.noreply.github.com>
2024-03-30 12:27:15 +0000

    Conversion & operation

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+let score = "33";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+/*
+"33" => 33
+"33abc" => NaN(Not a Number) But its type is Number
+true => 1; False => 0;
+*/
+
+let isLoggedIn = 1
+
+let booleanIsloggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsloggedIn);
+
+/*
+1 => true; 0 => false;
+"" => false
+"Dhruv" => true
+*/
+
+//**********************Operation*********************** 
+
+let value = 5
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "Dhruv"
+let str2 = " Singhal"
+
+let str3 = str1 + str2
+console.log(str3);
