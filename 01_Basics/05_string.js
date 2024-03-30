4b1fcc2415373448f5b7373b6b24484167d3a067
Dhruv Singhal <108335481+Dhruvcodelover@users.noreply.github.com>
2024-03-30 16:22:04 +0000

    comparsion

---
+++ 01_Basics/05_string.js
@@ -0,0 +1,28 @@
+const name = "Dhruv"
+const repoCount = 5
+//console.log(name + repoCount + "Value");  not recomended 
+
+// console.log(`Hello my name is ${name} and my repo Count is ${repoCount}`);
+
+const gameName = new String('Dhruv-Sing-hal')
+
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('r'));
+
+const newStringOne = "  Dhruv     "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://Dhruv.com/Dhruv%69Singhal"
+
+
+console.log(url.replace('%69','-'));
+
+console.log(url.includes('Dhruv'));
+
+console.log(gameName.split('-'));
